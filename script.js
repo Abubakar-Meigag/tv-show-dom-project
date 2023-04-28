351e6a09a4a908a4244bf13719051c22b36cb603
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-04-28 19:50:35 +0100

    first update
    - add div to contain all episodes
    - create layout as grid
    - add some style for css and still not finished
    - initialize some logic to let function work properly

--- script.js
+++ script.js
@@ -7,6 +7,39 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  //my code
+
+  let allEp = document.getElementById("allEp");
+
+  for (let list of episodeList) {
+    let episodeDiv = document.createElement("div");
+    // let title = document.createElement("h2");
+    // let img = document.createElement("img");
+    // let p = document.createElement("p");
+
+    episodeDiv.classList.add("newDiv");
+
+    episodeDiv.innerHTML = `
+      <h2>${list.name} - S${list.season
+      .toString()
+      .padStart(2, "0")}E${list.number.toString().padStart(2, "0")}</h2>
+      <img src="${list.image.medium}">
+      <p>${list.summary}</p>
+    `;
+
+    // let title = episodeDiv.querySelector('h2');
+    // let img = episodeDiv.querySelector('img');
+    // let p = episodeDiv.querySelector('p');
+
+    // title.innerHTML = `${list.name} - S${list.season
+    //   .toString()
+    //   .padStart(2, "0")}E${list.number.toString().padStart(2, "0")}`;
+    // img.src = list.image.medium;
+    // p = list.summary;
+
+    allEp.appendChild(episodeDiv);
+  }
 }
 
 window.onload = setup;
