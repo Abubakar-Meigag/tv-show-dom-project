16170f8025260dc952012f779eaf755ca37c818f
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-05-05 20:57:54 +0100

    create option
    create option and call option in console by change

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //You can edit ALL of the code here
+let allEpisodes = getAllEpisodes();
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 // create all episodes level 100
+// let episodeList = makePageForEpisodes();
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
@@ -31,6 +34,9 @@ function makePageForEpisodes(episodeList) {
     allEp.appendChild(episodeDiv);
   });
 }
+  
+
+
 
 // create search bar level 200
 
@@ -69,9 +75,28 @@ function episodesSearch() {
   }
 }
 
+// create Episode Selector level 300
 
+let selector = document.getElementById("cards");
+selector.innerHTML = `<option value="Please choose episode">Please choose episode</option>`;
 
+let optionList = allEpisodes.forEach(el => {
+  let options = document.createElement('option')
+  options.value = el.name;
+  options.text = el.name
+  
+  selector.appendChild(options)
 
+})
+  selector.addEventListener("change", (ele) => {
+    let val = ele.target.value;
+    console.log(val);
+  });
 
+function episodesSelector(){
+
+}
 
 window.onload = setup;
+
+
